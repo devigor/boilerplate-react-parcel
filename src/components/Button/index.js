16b27eb8c85800f68893651731a7ecc792672971
Igor Oliveira <devigor.tere@gmail.com>
2018-09-09 10:54:28 -0300

    Estrutura inicial

---
+++ src/components/Button/index.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Button = styled.button`
+  width: 150px;
+  height: 25px;
+  border: none;
+  border-radius: 5%;
+  background-color: tomato;
+  color: #fff
+`
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+import { Button } from './components/Button'
+
+render(
+  <Button>
+    React and Parcel!
+  </Button>,
+  document.getElementById('root')
+)
